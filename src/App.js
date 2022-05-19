8bbc370cbc3bdc1504d4ef884572c4cfdcade2f9
richashah03 <61013203+richabot@users.noreply.github.com>
2022-05-20 01:29:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Whole } from "./components/Whole";
+
+
+function App() {
+  return (
+   <div>
+   <Whole/>
+   </div>
+  );
+}
+
+export default App;
